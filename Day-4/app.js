54145d41a5e387f4b074c1e37ea8af54e16a3a03
shivam-dubey14 <158604672+shivam-dubey14@users.noreply.github.com>
2025-12-11 05:30:49 +0000

    Day-4

---
+++ Day-4/app.js
@@ -0,0 +1,8 @@
+// 
+let mydate = new Date();   // 'Date' must be capitalized
+console.log(mydate.toString());
+console.log(mydate.toISOString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toLocaleTimeString());
+console.log(mydate.toUTCString());
+
